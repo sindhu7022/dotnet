165dbf5deb38d52c5d406fbe2ee4dc16cb02ab20
sindhu7022 <76683190+sindhu7022@users.noreply.github.com>
2022-08-24 15:46:47 +0530

    Add files via upload

---
+++ multiplication/Program.cs
@@ -0,0 +1,34 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class multiplicationofnum
+    {
+        public static void Main()
+        {
+            int n1, n2, n3;
+
+
+            Console.WriteLine("enter first number");
+            n1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("enter sec number");
+            n2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("enter third number");
+            n3 = Convert.ToInt32(Console.ReadLine());
+            int tot = n1 * n2 * n3;
+            Console.WriteLine($"multiplication is {tot}");
+        }
+    }
+}
+
+
+
+
+
+
+
